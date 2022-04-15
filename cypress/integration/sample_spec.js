80aa91d7af56805eda1b8104189b869cfe1b6c38
corbatsh <bortlikkamil@gmail.com>
2022-04-15 14:22:50 +0200

    First test completed

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,15 @@
 describe('my First Test', () => {
-    it('Does not do much', () => {
-        expect(true).to.equal(true)
+    it('Visits the Kitchen Sink', () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com')
     })
 })
