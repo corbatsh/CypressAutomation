d335fcf2bcd5443337dfc2b50e98e5880521c043
corbatsh <bortlikkamil@gmail.com>
2022-04-30 12:34:44 +0200

    document property check

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -19,4 +19,17 @@ describe('Write / Read data to JSON / Text file', () => {
             .readFile('log.txt')
             .should('eq', 'Hello World')
     })
+
+    it('Should read and verify browser document content', () => {
+        cy.visit('https://www.example.com')
+        cy.wait(2000)
+        cy
+            .document()
+            .its('contentType')
+            .should('eq', 'text/html')
+
+        cy.document()
+            .should('have.property', 'charset')
+            .and('eq', 'UTF-8')
+    })
 })
