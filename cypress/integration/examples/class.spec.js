2a5eff9d1cbe97d2869bd0795a89278a5ed868e8
corbatsh <bortlikkamil@gmail.com>
2022-04-30 13:05:10 +0200

    classes and before&after hooks

---
+++ cypress/integration/examples/class.spec.js
@@ -0,0 +1,49 @@
+class BasePage {
+    static loadHomePage() {
+        cy.visit('https://devexpress.github.io/testcafe/example/')
+    }
+
+    static wait(number) {
+        cy.wait(number)
+    }
+}
+
+class HomePage extends BasePage {
+    static scrollToBottom() {
+        cy.get('#submit-button').scrollIntoView()
+    }
+
+    static scrollToTop() {
+        cy.get('header').scrollIntoView()
+    }
+}
+
+describe('Abstraction with Classes', () => {
+    before(() => {
+        // runs before all tests inside describe
+        // setup test data or test context
+        // seed or reset the database
+        HomePage.loadHomePage()
+    })
+
+    after(() => {
+        // runs after all tests inside describe block are done
+        // test clean up
+        // clean cookies or localStorage
+    })
+
+    beforeEach(() => {
+        // runs before each "it" block in the describe
+    })
+
+    afterEach(() => {
+        // runs after each "it" block in the describe
+    })
+
+    it('Should scroll down and up on the page', () => {
+        HomePage.scrollToBottom()
+        HomePage.wait(3000)
+        HomePage.scrollToTop()
+        HomePage.wait(3000)
+    })
+})
